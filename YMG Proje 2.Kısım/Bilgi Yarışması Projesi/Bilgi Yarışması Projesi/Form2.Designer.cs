cf8d332dfdd579cd3db039dd37efbfcad8a6f35f
zirem11 <89360252+zirem11@users.noreply.github.com>
2023-05-25 02:39:05 +0300

    Add files via upload
    5.soruya kadar tamamlandı.

---
+++ YMG Proje 2.Kısım/Bilgi Yarışması Projesi/Bilgi Yarışması Projesi/Form2.Designer.cs
@@ -0,0 +1,240 @@
+﻿
+namespace Bilgi_Yarışması_Projesi
+{
+    partial class frmsoru1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.btnA = new System.Windows.Forms.Button();
+            this.btnB = new System.Windows.Forms.Button();
+            this.btnC = new System.Windows.Forms.Button();
+            this.btnD = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblsoruno = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.lbldogru = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.lblyanlis = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Font = new System.Drawing.Font("Segoe Print", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.richTextBox1.Location = new System.Drawing.Point(0, -1);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(535, 280);
+            this.richTextBox1.TabIndex = 0;
+            this.richTextBox1.Text = "";
+            // 
+            // btnA
+            // 
+            this.btnA.BackColor = System.Drawing.Color.White;
+            this.btnA.Font = new System.Drawing.Font("Segoe Print", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnA.Location = new System.Drawing.Point(19, 301);
+            this.btnA.Name = "btnA";
+            this.btnA.Size = new System.Drawing.Size(233, 49);
+            this.btnA.TabIndex = 1;
+            this.btnA.Text = "A";
+            this.btnA.UseVisualStyleBackColor = false;
+            // 
+            // btnB
+            // 
+            this.btnB.BackColor = System.Drawing.Color.White;
+            this.btnB.Font = new System.Drawing.Font("Segoe Print", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnB.Location = new System.Drawing.Point(284, 301);
+            this.btnB.Name = "btnB";
+            this.btnB.Size = new System.Drawing.Size(233, 49);
+            this.btnB.TabIndex = 2;
+            this.btnB.Text = "B";
+            this.btnB.UseVisualStyleBackColor = false;
+            // 
+            // btnC
+            // 
+            this.btnC.BackColor = System.Drawing.Color.White;
+            this.btnC.Font = new System.Drawing.Font("Segoe Print", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnC.Location = new System.Drawing.Point(19, 369);
+            this.btnC.Name = "btnC";
+            this.btnC.Size = new System.Drawing.Size(233, 49);
+            this.btnC.TabIndex = 3;
+            this.btnC.Text = "C";
+            this.btnC.UseVisualStyleBackColor = false;
+            // 
+            // btnD
+            // 
+            this.btnD.BackColor = System.Drawing.Color.White;
+            this.btnD.Font = new System.Drawing.Font("Segoe Print", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnD.Location = new System.Drawing.Point(284, 369);
+            this.btnD.Name = "btnD";
+            this.btnD.Size = new System.Drawing.Size(233, 49);
+            this.btnD.TabIndex = 4;
+            this.btnD.Text = "D";
+            this.btnD.UseVisualStyleBackColor = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe Print", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(560, 30);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(100, 33);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Soru No:";
+            // 
+            // lblsoruno
+            // 
+            this.lblsoruno.AutoSize = true;
+            this.lblsoruno.Font = new System.Drawing.Font("Segoe Print", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblsoruno.Location = new System.Drawing.Point(681, 30);
+            this.lblsoruno.Name = "lblsoruno";
+            this.lblsoruno.Size = new System.Drawing.Size(29, 33);
+            this.lblsoruno.TabIndex = 6;
+            this.lblsoruno.Text = "0";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Segoe Print", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label3.Location = new System.Drawing.Point(580, 79);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(80, 33);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Doğru:";
+            // 
+            // lbldogru
+            // 
+            this.lbldogru.AutoSize = true;
+            this.lbldogru.Font = new System.Drawing.Font("Segoe Print", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lbldogru.Location = new System.Drawing.Point(681, 79);
+            this.lbldogru.Name = "lbldogru";
+            this.lbldogru.Size = new System.Drawing.Size(29, 33);
+            this.lbldogru.TabIndex = 8;
+            this.lbldogru.Text = "0";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Segoe Print", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label5.Location = new System.Drawing.Point(583, 135);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(77, 33);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "Yanlış:";
+            // 
+            // lblyanlis
+            // 
+            this.lblyanlis.AutoSize = true;
+            this.lblyanlis.Font = new System.Drawing.Font("Segoe Print", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblyanlis.Location = new System.Drawing.Point(681, 135);
+            this.lblyanlis.Name = "lblyanlis";
+            this.lblyanlis.Size = new System.Drawing.Size(29, 33);
+            this.lblyanlis.TabIndex = 10;
+            this.lblyanlis.Text = "0";
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.White;
+            this.button1.Font = new System.Drawing.Font("Segoe Print", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button1.Location = new System.Drawing.Point(552, 189);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(213, 50);
+            this.button1.TabIndex = 11;
+            this.button1.Text = "Sıradaki Soru";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe Print", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.Location = new System.Drawing.Point(560, 302);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(72, 33);
+            this.label2.TabIndex = 12;
+            this.label2.Text = "dogru";
+            this.label2.Visible = false;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Segoe Print", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label4.Location = new System.Drawing.Point(681, 302);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(69, 33);
+            this.label4.TabIndex = 13;
+            this.label4.Text = "yanlis";
+            this.label4.Visible = false;
+            // 
+            // frmsoru1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.ClientSize = new System.Drawing.Size(804, 471);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.lblyanlis);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.lbldogru);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.lblsoruno);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnD);
+            this.Controls.Add(this.btnC);
+            this.Controls.Add(this.btnB);
+            this.Controls.Add(this.btnA);
+            this.Controls.Add(this.richTextBox1);
+            this.Name = "frmsoru1";
+            this.Text = "Form2";
+            this.Load += new System.EventHandler(this.frmsoru1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button btnA;
+        private System.Windows.Forms.Button btnB;
+        private System.Windows.Forms.Button btnC;
+        private System.Windows.Forms.Button btnD;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblsoruno;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label lbldogru;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label lblyanlis;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label4;
+    }
+}
---
+++ YMG Proje 2.Kısım/Bilgi Yarışması Projesi/Bilgi Yarışması Projesi/Form2.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Bilgi_Yarışması_Projesi
+{
+    public partial class frmsoru1 : Form
+    {
+        public frmsoru1()
+        {
+            InitializeComponent();
+        }
+
+        private void frmsoru1_Load(object sender, EventArgs e)
+        {
+
+        }
+        int soruno = 0; int dogru = 0; int yanlis = 0;
+        private void button1_Click(object sender, EventArgs e)
+        {
+            soruno++;
+            lblsoruno.Text = soruno.ToString();
+
+            if(soruno==1)
+            {
+                richTextBox1.Text = "Yazıyı icat eden ilk çağ uygarlığı hangisidir?";
+                btnA.Text = "Hititler";
+                btnB.Text = "Uygurlar";
+                btnC.Text = "Sümerler";
+                btnD.Text = "Urartular";
+            }
+
+            
+
+            if (soruno== 2)
+            {
+                richTextBox1.Text = "İstiklal Marşımızın bestecisi kimdir?";
+                btnA.Text = "Osman Zeki Güngör";
+                btnB.Text = "Mehmet Akif Ersoy";
+                btnC.Text = "Yahya Kemal Beyatlı";
+                btnD.Text = "Hamdullah Suphi Tanrıöver";
+            }
+
+
+            if (soruno == 3)
+            {
+                richTextBox1.Text = "5 sayısının türevi nedir?";
+                btnA.Text = "1";
+                btnB.Text = "2";
+                btnC.Text = "3";
+                btnD.Text = "0";
+            }
+
+
+            if (soruno == 4)
+            {
+                richTextBox1.Text = "Akşam namazı kaç rekattır?";
+                btnA.Text = "4";
+                btnB.Text = "10";
+                btnC.Text = "5";
+                btnD.Text = "13";
+            }
+
+            if (soruno == 5)
+            {
+                richTextBox1.Text = "Rusya'nın cumhurbaşkanı kimdir?";
+                btnA.Text = "Vladimir Putin";
+                btnB.Text = "Emmanuel Macron";
+                btnC.Text = " Sergio Mattarella";
+                btnD.Text = "Dina Boluarte";
+            }
+
+           
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
